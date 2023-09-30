1b44c02c7d2876ba50403ce60e7070de57c583d7
Thi <anhthix7c@gmail.com>
2023-10-01 02:50:32 +0700

    version-6

--- libs/fetchApi/todoApi.ts
+++ libs/fetchApi/todoApi.ts
@@ -1,15 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 
 export const getAllTodo = async (): Promise<Todo[]> =>
-  await fetch("http://localhost:3000/api/todo").then((res) => res.json());
+  await fetch("/api/todo").then((res) => res.json());
 export const getTodo = async (id: string): Promise<Todo> =>
-  await fetch(`http://localhost:3000/api/todo/${id}`).then((res) => res.json());
+  await fetch(`/api/todo/${id}`).then((res) => res.json());
 export const deleteTodo = async (id: string) =>
-  await fetch(`http://localhost:3000/api/todo?id=${id}`, {
+  await fetch(`/api/todo?id=${id}`, {
     method: "DELETE",
   });
 export const updateTodo = async ({ id, todo }: { id: string; todo: Todo }) =>
-  await fetch(`http://localhost:3000/api/todo/${id}`, {
+  await fetch(`/api/todo/${id}`, {
     method: "PUT",
     headers: {
       "Content-type": "application/json",
@@ -18,7 +18,7 @@ export const updateTodo = async ({ id, todo }: { id: string; todo: Todo }) =>
   });
 
 export const createTodo = async (todo: TodoPost): Promise<TodoPost> =>
-  await fetch("http://localhost:3000/api/todo", {
+  await fetch("/api/todo", {
     method: "POST",
     headers: {
       "Content-type": "application/json",
