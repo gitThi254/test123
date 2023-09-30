0a6e383179dba359294c97cb94b68de6d7a9ce3a
Thi <anhthix7c@gmail.com>
2023-10-01 02:17:11 +0700

    version-2

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import AddTodo from "@/components/AddTodo";
 import TodoList from "@/components/TodoList";
 import Image from "next/image";
+import mongoose from "mongoose";
 
 export default function Home() {
+  const id = mongoose.connection.readyState;
   return (
     <main className=''>
+      {id}
       <AddTodo />
       <TodoList />
     </main>
